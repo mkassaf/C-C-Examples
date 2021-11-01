f95057729c2964fabdcc60fd59834f5177bfe734
Mustafa Assaf <mustafakassaf@gmail.com>
2021-11-01 14:19:35 +0200

    Update LabStrcure.cpp

--- LabStrcure.cpp
+++ LabStrcure.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include <string>
-using namespace std ;
-
-	struct Book {
+#include<string.h>
+using namespace std;
+struct Book {
 		int id; 
 		char name[50];
 		float price;
@@ -15,22 +14,30 @@ using namespace std ;
 		Book * books;
 	};
 	Book findBook(const BookStore bookStore, const char* name){
+	    
 		for (int  i = 0; i < bookStore.size; i++) {
-			if(strcomp(bookStore.books[i], name) == 0 ){
+			if(strcmp(bookStore.books[i].name, name) == 0 ){
 				return bookStore.books[i];
 			}
 		}
-		
+		Book tmp = {-1};
+		return tmp;
 	}
-	int main () {
-		BookStore bookStore = {1, "An Najah Books", "IT", 10};
+int main(){
+        BookStore bookStore = {1, "An Najah Books", "IT", 10};
 		bookStore.books = new Book[bookStore.size];
 		bookStore.books[0] = Book{1, "Java", 59.2};
-		bookStore.books[0] = Book{2, "Software enginnering", 89.2};
-		bookStore.books[0] = Book{3, "C/C++", 99.9};
-		bookStore.books[0] = Book{4, "PHP", 12.0};
-		bookStore.books[0] = Book{5, "Web programming", 50};
+		bookStore.books[1] = Book{2, "Software enginnering", 89.2};
+		bookStore.books[2] = Book{3, "C/C++", 99.9};
+		bookStore.books[3] = Book{4, "PHP", 12.0};
+		bookStore.books[4] = Book{5, "Web programming", 50};
 	    Book book = findBook(bookStore, "C/C++");
+	    cout << book.id <<endl;
+	    if(book.id != -1){
+	        cout << book.id << " | "<< book.name << " | " << book.price<<endl;
+	    } else {
+            cout<<"The book not found!"<< endl;   
+	    }
 
-		return 0;
-	}
+    return 0;
+}
